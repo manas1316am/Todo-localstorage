35cc4d08a947d61edce39256b7602016737f71cf
Manas Kumar <65833852+manas1316am@users.noreply.github.com>
2020-08-09 08:25:23 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import {Container} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
+import Todos from "./Components/Todos"
+import TodoForm from "./Components/TodoForm"
+
+const App = () => {
+  const [todos, settodos] = useState([])
+  // Array is compulosry in useEffect
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos")
+    console.log({localStorage});
+    if(localTodos){
+      settodos(JSON.parse(localTodos))
+    }
+  }, [])
+
+  const addTodos = async todo => {
+    settodos([...todos, todo])
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]); 
+
+  const markComplete = id => {
+    settodos(todos.filter(todo => todo.id !== id))
+  }
+
+  return (
+    <Container fluid>
+      <h1>Todo with Local Storage</h1>
+      <Todos todos={todos} markComplete = {markComplete}/>
+      <TodoForm addTodos={addTodos} />
+    </Container>
+  )
+
+}
+
+export default App;
